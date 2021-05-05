17bbdc38e9de2287145891d224a5c292058fdf26
Amit P Naik <amit.naik8103@gmail.com>
2021-05-05 11:35:56 +0530

    feat: sample project

---
+++ src/SampleApplication.Api/Controllers/NamesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApplication.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NamesController : ControllerBase
+    {
+
+        private readonly ILogger<NamesController> _logger;
+
+        public NamesController(ILogger<NamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { Environment.MachineName };
+        }
+    }
+}
